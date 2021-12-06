bc969a063d2795efd1baba770bd3e8456d753a0d
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-06 15:57:03 +0000

    Added delete todo.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [todoText, setTodoText] = useState('') //text which entering to the box
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('') //the value in the input
 
-  const [notStartedYetTodos, setNotStartedYetTodos] = useState([])
-  const [inProgressTodos, setInProgressTodos] = useState([''])
-  const [completeTodos, setCompleteTodos] = useState([''])
-
-  const onChangeTodoText = (e) => setTodoText(e.target.value)
-  const onClickAdd = () => {
-    if(todoText !== "") {
-      setNotStartedYetTodos([...notStartedYetTodos, {id: notStartedYetTodos.length + 1, text:todoText}])
-      console.log(notStartedYetTodos);
-      setTodoText('')
+  const onChangeInput = (e) => setTodo(e.target.value)//what the differences between this and
+  const onClickAdd = (e) => {
+    e.preventDefault()//this? Do i need both?
+    if(todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo
+        }
+      ])
+      setTodo('')
     }
   }
-  const onClickDelete = (index) => {
-    const newTodos = [...notStartedYetTodos]
-    newTodos.splice(index, 1)
-    setNotStartedYetTodos(newTodos) //未完了のnewTodoで更新してあげましょう、がわからない
-    alert('Are you sure you want to delete?')
+  const onClickDelete = (id) => {//...why is it ok to be just id instead of todo.id
+    const removeItem = todos.filter((todo) => {
+      return todo.id !== id
+    })
+    setTodos(removeItem)
+    // alert('Are you sure you want to delete?')
   }
 
 
@@ -27,48 +31,28 @@ const App = () => {
   <>
   <h1>Todo React App</h1>
   <div className="input_area">
-    <input type="text" label="title" value={todoText} onChange={onChangeTodoText}/>
-    <button onClick={onClickAdd}>Add</button>
+    <form> {/* do I need <form></form>? */}
+      <input 
+      name="todo"
+      type="text"
+      placeholder="Create a new todo"
+      value={todo}
+      onChange={onChangeInput} 
+      />
+      <button onClick={onClickAdd}>Add</button>
+    </form>
   </div>
 
-  <div className="not_started_yet_area">
-    <ul>
-      {notStartedYetTodos.map((todo) => {
+  <div className="">
+    <ul className="todo-list">
+      {todos.map((todo) => {
         return (
-          <li key={todo}>{todo}
-            <input type="text" value={todo.value}
-            
-            />
-            <button>Edit</button>
-            <button onClick={() =>{onClickDelete()}}>Delete</button>
+          //   <button>Edit</button>
+          <li key={todo.id}>{todo.text}
+            <button onClick={() =>{onClickDelete(todo.id)}}>Delete</button>
           </li>
         )
       })}
-    </ul>
-  </div>
-  <div className="in_progress_area">
-    <ul>
-      {inProgressTodos.map((todo) => {
-      return (
-        <li key={todo}>{todo}
-          <button>Edit</button>
-          <button>Delete</button>
-        </li>
-      )
-      })}
-    </ul>
-  </div>
-  <div className="complete_area">
-    <ul>
-      {completeTodos.map((todo) => {
-        return (
-          <li key={todo}>{todo}
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-        )
-      })
-      }
     </ul>
   </div>
   </>
