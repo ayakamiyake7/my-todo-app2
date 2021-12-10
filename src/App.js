13e413e47109ad5a430ff382a8352b3b0a2be519
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-10 11:10:52 +0000

    Add edit button.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const [todo, setTodo] = useState('') //the value in the input
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState(""); //the value in the input
 
-  const [isEditing, setIsEditing] = useState(false)
-  const [currentTodo, setCurrentTodo] = useState({}) //why is this object state? currentTodo is multiple?
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({}); //why is this object state? currentTodo is multiple?
 
-  const handleInputChange = (e) => setTodo(e.target.value)//what is this?
-  const handleEditInputChange = (e) => setCurrentTodo({...currentTodo, text: e.target.value}) //
-  
-  const handleAdd = (e) => {
-    e.preventDefault()
-    if(todo !== "") {
+  const handleInputChange = (e) => setTodo(e.target.value);
+  const handleEditInputChange = (e) =>
+    setCurrentTodo({ ...currentTodo, text: e.target.value }); //？
+
+  const handleAdd = () => {
+    // e.preventDefault();
+    if (todo !== "") {
       setTodos([
         ...todos,
         {
@@ -20,99 +21,109 @@ const App = () => {
           text: todo,
           //isDone: false,
           //state: 'incomplete'
-        }
-      ])
-      setTodo('')
+        },
+      ]);
+      setTodo("");
     }
-  }
+  };
 
-  const handleUpdate = (id, updatedTodo) => {//why does 'updatedTodo' appear?
+  const handleUpdate = (id, updatedTodo) => {
+    //why does 'updatedTodo' appear?
     const updatedItem = todos.map((todo) => {
       return todo.id === id ? updatedTodo : todo;
-    })
-    setIsEditing(false)
-    setTodos(updatedItem)
-    // e.preventDefault()
-    // setIsEditing(true)
-    // setTodos([
-    //   ...todos,
-    //   {
-    //     id: currentTodo.id,
-    //     text: currentTodo.text,
-    //     //isDone: false,
-    //     // state: 'incomplete'
-    //   }
-    // ])
-  }
+    });
+    setIsEditing(false);
+    setTodos(updatedItem);
+  };
 
-  const handleEditOnChange = () => {// where should i write handleEditOnChange ---sample:form
-    handleUpdate(currentTodo.id, currentTodo)// nazo?
-  }
+  const handleEditOnChange = (e) => {
+    // e.preventDefault();
+    handleUpdate(currentTodo.id, currentTodo); //?
+  };
 
   const handleClickDelete = (id) => {
     const removeItem = todos.filter((todo) => {
-      return todo.id !== id
-    })
-    setTodos(removeItem)
-  }
+      return todo.id !== id;
+    });
+    setTodos(removeItem);
+  };
 
   const handleEditClick = (todo) => {
-    setIsEditing(true)
-    setCurrentTodo({...todo})//what is this?
-  }
-
+    setIsEditing(true);
+    setCurrentTodo(todo); //what is this?
+    console.log(todo);
+    console.log({ todo });
+    console.log({ ...todo });
+  };
 
   return (
-  <>
-  <h1>Todo React App</h1>  
-  {isEditing ? (
-  <div onSubmit={handleEditOnChange}> {/**not here...*/}
-    <div className="input_area">
-      <input 
-      name="todo"
-      type="text"
-      placeholder="Create a new todo"
-      value={todo}
-      onChange={handleInputChange}
-      />
-      <button onClick={handleAdd}>Add</button>
-    </div>
-    <div className="content">
-      <input 
-      name="editTodo"
-      type="text"
-      placeholder="Edit todo"
-      value={currentTodo.text}
-      onChange={handleEditInputChange}
-      />
-      <button onClick={handleUpdate}>Update</button>
-      <button onClick={() => setIsEditing(false)}>Cancel</button>
-    </div>
-  </div>
-  ) : (
-    <div className="input_area">
-      <input 
-      name="todo"
-      type="text"
-      placeholder="Create a new todo"
-      value={todo}
-      onChange={handleInputChange}
-      />
-      <button onClick={handleAdd}>Add</button>
-    </div>
-  )}
-  <ul className="todo-list">
-      {todos.map((todo) => {
-        return (
-          <li key={todo.id}>{todo.text}
-            <button onClick={() => {handleEditClick(todo)}}>Edit</button>{/**why entire todo? */}
-            <button onClick={() => {handleClickDelete(todo.id)}}>Delete</button>
-          </li>
-        )
-      })}
-    </ul>
-  </>
-  )
-}
+    <>
+      <h1>Todo React App</h1>
+      {isEditing ? (
+        <>
+          {/**not here...*/}
+          <div className="input_area">
+            <input
+              name="todo"
+              type="text"
+              placeholder="Create a new todo"
+              value={todo}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleAdd}>Add</button>
+          </div>
+          <div className="content">
+            <form onSubmit={handleEditOnChange}>
+              <input
+                name="editTodo"
+                type="text"
+                placeholder="Edit todo"
+                value={currentTodo.text}
+                onChange={handleEditInputChange}
+              />
+              <input type="submit" value="update" />
+              <button onClick={() => setIsEditing(false)}>Cancel</button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <div className="input_area">
+          <input
+            name="todo"
+            type="text"
+            placeholder="Create a new todo"
+            value={todo}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleAdd}>Add</button>
+        </div>
+      )}
+      <ul className="todo-list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.text}
+              <button
+                onClick={() => {
+                  handleEditClick(todo);
+                }}
+              >
+                Edit
+              </button>
+              {/**why entire todo? */}
+              <button
+                onClick={() => {
+                  handleClickDelete(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default App;
