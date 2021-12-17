fce327ef6ca6690d83ef6232ba3d44d357bd2488
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-17 22:44:13 +0000

    Fix status. #1

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import "./style.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState(""); //the value in the input
+  const [todo, setTodo] = useState(""); ////the value in the input
 
   const [currentStatus, setCurrentStatus] = useState("notStarted");
   const [isEditing, setIsEditing] = useState(false);
-  const [currentTodo, setCurrentTodo] = useState({}); //why is this object state? currentTodo is multiple?
+  const [currentTodo, setCurrentTodo] = useState({}); ////why is this object state? currentTodo is multiple?
 
   const handleInputChange = (e) => setTodo(e.target.value);
   const handleEditInputChange = (e) =>
-    setCurrentTodo({ ...currentTodo, text: e.target.value }); //？
+    setCurrentTodo({ ...currentTodo, text: e.target.value }); ////？
 
   const handleAdd = () => {
-    // e.preventDefault();
+    //// e.preventDefault();
     if (todo !== "") {
       setTodos([
         ...todos,
         {
           id: todos.length + 1,
           text: todo,
-          //isDone: false,
+          ////isDone: false,
           status: "notStarted",
         },
       ]);
@@ -30,7 +30,7 @@ const App = () => {
   };
 
   const handleUpdate = (id, updatedTodo) => {
-    //why does 'updatedTodo' appear?
+    ////why does 'updatedTodo' appear?
     const updatedItem = todos.map((todo) => {
       return todo.id === id ? updatedTodo : todo;
     });
@@ -39,7 +39,7 @@ const App = () => {
   };
 
   const handleEditOnChange = () => {
-    // e.preventDefault();
+    //// e.preventDefault();
     handleUpdate(currentTodo.id, currentTodo); //?
   };
 
@@ -52,14 +52,25 @@ const App = () => {
 
   const handleEditClick = (todo) => {
     setIsEditing(true);
-    setCurrentTodo(todo); //what is this?
+    setCurrentTodo(todo); ////what is this?
     console.log(todo);
     console.log({ todo });
     console.log({ ...todo });
   };
 
-  const handleStatusChange = () => {};
-  console.log("rendering");
+  const handleStatusChangeNotStarted = (e) => {
+    e.preventDefault();
+    setCurrentStatus(e.target.value);
+    const a = todos.filter((todo) => {
+      return todo.status === "notStarted";
+    });
+    setTodos(a);
+    console.log(a);
+  };
+
+  useEffect(() => {
+    // const changingStatus =
+  });
 
   return (
     <>
@@ -112,141 +123,126 @@ const App = () => {
         </>
       )}
 
-      <ul className="todo-list">
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              {todo.text}
-              <select>
-                <option value="notStarted">Not Started</option>
-                <option value="inProgress">In Progress</option>
-                <option value="done">Done</option>
-              </select>
-              <button
-                onClick={() => {
-                  handleEditClick(todo);
-                }}
-              >
-                Edit
-              </button>
-              {/**why entire todo? */}
-              <button
-                onClick={() => {
-                  handleClickDelete(todo.id);
-                }}
-                className="content-grey"
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
       <div className="flex">
         <div className="item">
           <div id="notStarted" className="todo-place">
             <h2>Not Started</h2>
+            <ul className="todo-list">
+              {todos.map((todo) => {
+                return (
+                  <li key={todo.id}>
+                    {todo.text}
+
+                    <select
+                      value={currentStatus.status}
+                      onChange={handleStatusChangeNotStarted}
+                    >
+                      <option value="notStarted">Not Started</option>
+                      <option value="inProgress">In Progress</option>
+                      <option value="done">Done</option>
+                    </select>
+
+                    <button
+                      onClick={() => {
+                        handleEditClick(todo);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => {
+                        handleClickDelete(todo.id);
+                      }}
+                      className="content-grey"
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-          <ul className="todo-list">
-            {todos.filter((todo) => {
-              console.log("click");
-              return (
-                <div>ok</div>
-                // <li key={todo.id}>
-                //   {todo.text}
-                //   <select>
-                //     <option value="notStarted">Not Started</option>
-                //     <option value="inProgress">In Progress</option>
-                //     <option value="done">Done</option>
-                //   </select>
-                //   <button
-                //     onClick={() => {
-                //       handleEditClick(todo);
-                //     }}
-                //   >
-                //     Edit
-                //   </button>
-                //   <button
-                //     onClick={() => {
-                //       handleClickDelete(todo.id);
-                //     }}
-                //     className="content-grey"
-                //   >
-                //     Delete
-                //   </button>
-                // </li>
-              );
-            })}
-          </ul>
         </div>
-        {/* <div className="item">
+
+        <div className="item">
           <div id="inProgress" className="todo-place">
             <h2>In progress</h2>
+            <ul className="todo-list">
+              {todos.map((todo) => {
+                return (
+                  <li key={todo.id}>
+                    {todo.text}
+
+                    <select
+                      value={currentStatus.status}
+                      // onChange={handleStatusChange}
+                    >
+                      <option value="notStarted">Not Started</option>
+                      <option value="inProgress">In Progress</option>
+                      <option value="done">Done</option>
+                    </select>
+
+                    <button
+                      onClick={() => {
+                        handleEditClick(todo);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => {
+                        handleClickDelete(todo.id);
+                      }}
+                      className="content-grey"
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-          <ul className="todo-list">
-            {todos.map((todo) => {
-              return (
-                <li key={todo.id}>
-                  {todo.text}
-                  <select>
-                    <option value="notStarted">Not Started</option>
-                    <option value="inProgress">In Progress</option>
-                    <option value="done">Done</option>
-                  </select>
-                  <button
-                    onClick={() => {
-                      handleEditClick(todo);
-                    }}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleClickDelete(todo.id);
-                    }}
-                    className="content-grey"
-                  >
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
-        {/* <div className="item">
-          <div id="notStarted" className="todo-place">
-            <h2>Do</h2>
+        </div>
+
+        <div className="item">
+          <div id="done" className="todo-place">
+            <h2>Done</h2>
+            <ul className="todo-list">
+              {todos.map((todo) => {
+                return (
+                  <li key={todo.id}>
+                    {todo.text}
+
+                    <select
+                      value={currentStatus.status}
+                      // onChange={handleStatusChange}
+                    >
+                      <option value="notStarted">Not Started</option>
+                      <option value="inProgress">In Progress</option>
+                      <option value="done">Done</option>
+                    </select>
+
+                    <button
+                      onClick={() => {
+                        handleEditClick(todo);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => {
+                        handleClickDelete(todo.id);
+                      }}
+                      className="content-grey"
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-          <ul className="todo-list">
-            {todos.map((todo) => {
-              return (
-                <li key={todo.id}>
-                  {todo.text}
-                  <select>
-                    <option value="notStarted">Not Started</option>
-                    <option value="inProgress">In Progress</option>
-                    <option value="done">Done</option>
-                  </select>
-                  <button
-                    onClick={() => {
-                      handleEditClick(todo);
-                    }}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleClickDelete(todo.id);
-                    }}
-                    className="content-grey"
-                  >
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
+        </div>
       </div>
     </>
   );
